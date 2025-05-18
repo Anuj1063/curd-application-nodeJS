26e6c028ef585927f858271cda09b9a398c6783c
Anuj1063 <anujkumar11063@gmail.com>
2025-05-18 18:01:12 +0530

    aggeration change

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,108 +1,134 @@
-const UserModel = require('../models/user.model')
-const UserDetailsModel = require('../models/user_details.model')
+const UserModel = require("../models/user.model");
+const UserDetailsModel = require("../models/user_details.model");
 
 class StudentController {
+  async create(req, res) {
+    res.render("add");
+  }
 
-    async create(req, res) {
-        res.render('add');
+  async insert(req, res) {
+    let isEmailExists = await UserModel.find({
+      email: req.body.email,
+      isDeleted: false,
+    });
+
+    if (isEmailExists.length > 0) {
+      req.flash("error", "Email is already exists");
+      return res.redirect("/");
     }
 
-    async insert(req, res) {
-        let isEmailExists = await UserModel.find({
-            email: req.body.email,
-            isDeleted: false
-        });
-        
-        
-        if (isEmailExists.length > 0) {
-            req.flash("error", "Email is already exists");
-            return res.redirect("/");
+    let userObj = {
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      email: req.body.email,
+    };
+
+    let saveData = await UserModel.create(userObj);
+
+    if (saveData) {
+      let userDetailsObj = {
+        userId: saveData._id,
+        address: req.body.address,
+        bloodGroup: req.body.bloodGroup,
+      };
+
+      let userDetailsSave = await UserDetailsModel.create(userDetailsObj);
+
+      if (userDetailsSave) {
+        req.flash("success", "Data save successfully!");
+        return res.redirect("/list");
+      }
+    }
+  }
+
+  async list(req, res) {
+    let allData = await UserDetailsModel.aggregate([
+      {
+        $match: { isDeleted: false },
+      },
+      {
+        $lookup: {
+          from: "users",
+          localField: "userId",
+          foreignField: "_id",
+          as: "userDetails",
+        },
+      },
+      {
+        $unwind:"$userDetails"
+      },
+     {
+        $project:{
+            "firstName":"$userDetails.firstName",
+            "lastName":"$userDetails.lastName",
+            "email":"$userDetails.email",
+            "address":"$address",
+            "bloodGroup":"$bloodGroup"
         }
+     }
+    ]);
 
-        let userObj = {
-            firstName: req.body.firstName,
-            lastName: req.body.lastName,
-            email: req.body.email
-        }
+    res.render("list", {
+      allData,
+    });
+  }
 
-        let saveData = await UserModel.create(userObj);
+  async edit(req, res) {
+    let userData = await UserModel.findOne({
+      _id: req.params.id,
+      isDeleted: false,
+    });
+    let userData2 = await UserDetailsModel.find({
+      userId: req.params.id,
+    });
+    console.log(userData2);
+    res.render("edit", {
+      userData,
+      userData2,
+    });
+  }
 
-        if (saveData) {
-            let userDetailsObj = {
-                userId: saveData._id,
-                address: req.body.address,
-                bloodGroup: req.body.bloodGroup
-            }
+  async update(req, res) {
+    let isEmailExists = await UserModel.find({
+      email: req.body.email,
+      isDeleted: false,
+      _id: { $ne: req.body.id },
+    });
+    if (isEmailExists.length > 0) {
+      req.flash("error", "Email is already exists");
+      return res.redirect(`/edit/${req.body.id}`);
+    }
+    let updateData = await UserModel.updateOne(
+      {
+        _id: req.body.id,
+      },
+      req.body
+    );
 
-            let userDetailsSave = await UserDetailsModel.create(userDetailsObj);
-
-            if (userDetailsSave) {
-                req.flash("success", "Data save successfully!");
-                return res.redirect("/list");
-            }
-        }
-
-
+    if (updateData) {
+      req.flash("success", "Data updated successfully!");
+      return res.redirect(`/list`);
     }
 
-    async list(req, res) {
-        let allData = await UserModel.find({ isDeleted: false });
-        res.render('list', {
-            
-            allData
-        })
+    console.log("Data updated successfully!", updateData);
+  }
 
+  async softDelete(req, res) {
+    let deleteData = await UserModel.updateOne(
+      {
+        _id: req.params.id,
+      },
+      {
+        isDeleted: true,
+      }
+    );
+    if (deleteData) {
+      res.redirect("/list");
     }
+    console.log("Data deleted successfully!", deleteData);
+  }
 
-    async edit(req, res) {
-        let userData = await UserModel.findOne({
-            _id: req.params.id,
-            isDeleted: false
-        });
-        let userData2 = await UserDetailsModel.find({
-            userId: req.params.id,
-        });
-                console.log(userData2)
-        res.render('edit', {
-            userData,userData2
-        })
-    }
-
-    async update(req, res) {
-        let isEmailExists = await UserModel.find({
-            email: req.body.email,
-            isDeleted: false,
-            _id: { $ne: req.body.id }
-        });
-        if (isEmailExists.length > 0) {
-            req.flash("error", "Email is already exists");
-            return res.redirect(`/edit/${req.body.id}`);
-        }
-        let updateData = await UserModel.updateOne({
-            _id: req.body.id
-        }, req.body);
-
-        if (updateData) {
-            req.flash("success", "Data updated successfully!");
-            return res.redirect(`/list`);
-        }
-
-        console.log("Data updated successfully!", updateData);
-    }
-
-    async softDelete(req, res) {
-
-        let deleteData = await UserModel.updateOne({
-            _id: req.params.id
-        }, {
-            isDeleted: true
-        });
-
-        console.log("Data deleted successfully!", deleteData);
-    }
-
-    //Home Task-> Hard delete
-
+  //Home Task-> Hard delete
 }
 
 module.exports = new StudentController();
